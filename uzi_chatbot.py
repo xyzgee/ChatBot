6650929ea6f86e0fe74ba8603b8ae6519c18e5a2
Geetesh <geeteshkm25@gmail.com>
2024-06-14 20:12:13 +0530

    Add files via upload

---
+++ uzi_chatbot.py
@@ -0,0 +1,11 @@
+print ("Hello! This is Uzi, Your Friendly Chatbot")
+name = input ("What's your name?")
+print(f"Nice to meet you {name}")
+age_input = input ("How old are you? ")
+age = int(age_input)
+bot_age = 3
+age_difference = age - bot_age
+print (f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+color_input = input ("What's your favorite color?")
+color = str(color_input)
+print (f"Oh, {color} is a beautiful color")
